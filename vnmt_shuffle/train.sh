2047745707d5aff89608267887f38d6725d90eb7
Masanori Misono <misono@weblab.t.u-tokyo.ac.jp>
2017-07-20 11:52:26 +0000

    change parameter

--- vnmt_shuffle/train.sh
+++ vnmt_shuffle/train.sh
@@ -3,5 +3,6 @@
 mkdir -p result
 
 #python train_vnmt.py --dim=256 --dim_word=256 --dimv=256 --decay_c=0.001 --lr=1.0 --modeldir=vnmt_shuffle/ --batchsize=32 --fine_tuning --finetunedir="nmt_shuffle" 2>&1 | tee result/train.log
-python train_vnmt.py --dim=256 --dim_word=256 --dimv=512 --decay_c=0.001 --lr=1.0 --modeldir=vnmt_shuffle2/ --batchsize=32 --fine_tuning --finetunedir="nmt_shuffle" 2>&1 | tee result/train2.log
+#python train_vnmt.py --dim=256 --dim_word=256 --dimv=512 --decay_c=0.001 --lr=1.0 --modeldir=vnmt_shuffle2/ --batchsize=32 --fine_tuning --finetunedir="nmt_shuffle" 2>&1 | tee result/train2.log
+python train_vnmt.py --dim=256 --dim_word=256 --dimv=256 --decay_c=0.0005 --lr=1.0 --modeldir=vnmt_shuffle/ --batchsize=32 --fine_tuning --finetunedir="nmt_shuffle" 2>&1 | tee result/train.log
 
